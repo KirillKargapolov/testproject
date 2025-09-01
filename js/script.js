8ab50327871d205f3fbd8ac333d2ff1c13e2b173
kirill <nightmareasgard@gmail.com>
2025-09-01 12:17:52 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,174 @@
+"use strict"
+
+// const vehicleBodyWidth = 5000;
+// const vehicleBodyLength = 4000;
+
+// ///
+
+// console.log("Ширина кузова автомобиля: " + vehicleBodyWidth +', длина: ' + vehicleBodyLength);
+
+
+// const upperCaseArray = ['vrurur', 'ectr', 'rtrtrt'].map(a => a.toUpperCase());
+
+// console.log(upperCaseArray);
+
+
+
+// const data = ['kirill kargapolov', '26', 'male', 'programmer', 'ann smith', '21', 'female', 'designer'];
+// const [name1, age1, gender1, profession1, name2, age2, gender2, profession2] = data;
+//     console.log(`Hello, I'm ${name1}, I'm ${age1} years old, I'm a ${gender1} and I'm a ${profession1}.`);
+//     console.log(`Hello, I'm ${name2}, I'm ${age2} years old, I'm a ${gender2} and I'm a ${profession2}.`);
+
+
+
+
+
+// Типы данных в JS
+
+// 1. number
+
+// let age = 25;
+// let height = 175.5;
+// let weight = 70;
+
+// 2. string
+
+// let name = "John";
+// let profession = "Developer";
+// let city = "New York";
+
+// 3. boolean
+
+// let isEmployed = true;
+// let hasDrivingLicense = false;
+
+// 4. undefined    
+
+// let middleName;
+
+// 5. null
+
+// let address = null;
+
+// 6. symbol   
+
+// let uniqueID = Symbol("id");
+
+// 7. bigint
+
+// let largeNumber = 1234567890123456789012345678901234567890n;
+
+
+// let user = {
+//     name: "John",
+//     age: 25,
+//     isEmployed: true
+// }
+
+// console.log(Object.values(user));
+
+
+// let user2 = {
+//     name: 'ann',
+//     age: '25',
+//     isFat: false
+// }
+
+// console.log(Object.values(user2));
+
+// let user3 = []
+
+
+
+// console.log(arr[2]);
+
+// const obj = {
+//     a: 1,
+//     b: 2,
+//     c: 3
+// };
+
+// console.log(obj['c']);
+
+// const arr = ['a', 'b', 'c'];
+
+// const arrObj = {
+//     gg: 'a',
+//     malevolence: 'b',
+//     indestructible: 'c'
+// };
+
+// arrObj['indestructible'] = 'd';
+
+// console.log(arrObj['indestructible']);
+
+// const obj = {
+//     first: 1,
+//     second: 2,
+//     third: 3,
+//     fourth: { 
+//         df: [1, 2, 3],
+//         def: {
+
+//         }
+//     }
+// };
+
+// obj.b = '4';
+
+// console.log(obj.b);
+
+// const numbers = [1, 2, 3];
+
+// console.log(typeof(numbers));
+
+// const num = null;
+// console.log(typeof(num));
+
+
+// alert("Hello");
+
+// const result = confirm("Are you here?");
+
+// console.log(result);
+
+// const answer = +prompt("Are you there?", "Yes");
+// console.log(typeof(answer));
+
+// const answers = [1, 2, 3];
+
+// // answers[0] = prompt('как ваше имя?', '')
+// // answers[1] = prompt('как ваша фамилия?', '')
+// // answers[2] = prompt('сколько вам лет?', '')
+
+// console.log(Array.isArray(answers));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/4`);
+
+// const user = 'Ivan';
+
+// alert(`Привет, ${user}!`);
+
+
+// console.log('arr' + ' - object');
+
+
+// console.log("arr" + " - object");
+
+
+// let nmbr = 'zero'
+
+// nmbr++;
+
+// console.log(nmbr);
+
+
+// const isChecked = true, 
+//       isClose = false;
+
+//       console.log(isChecked || isClose);
+      
+
+
